afdf2b78acbdd05238449f00397e859f1e95d4fe
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-07 14:17:05 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import Input from "./components/Input/Input";
+import { useState } from 'react'
+import ToDo from './Input'
+import ToDoForm from './Input.Item'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTask = (userInput) => {
+    if(userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2,9),
+        task: userInput,
+        complete: false
+      }
+      setTodos([...todos, newItem])
+    }
+  }
+
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)])
+  }
+
+  const handleToggle = (id) => {
+    setTodos([
+      ...todos.map((todo) => 
+        todo.id === id ? { ...todo, complete: !todo.complete } : {...todo }
+      )
+    ])
+  }
+
   return (
     <div className="App">
-      <Input />
+      <header>
+        <h1>Task list: {todos.length}</h1>
+      </header>
+      <ToDoForm addTask={addTask} />
+      {todos.map((todo) => {
+        return (
+          <ToDo
+            todo={todo}
+            key={todo.id}
+            toggleTask={handleToggle}
+            removeTask={removeTask}
+            />
+        )
+      })}
     </div>
   );
 }
