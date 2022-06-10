2c940d74ae24fcc0615fc19c2fbdffa9be8847db
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-10 11:14:53 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,51 +1,101 @@
-import { useState } from 'react'
-import ToDo from './Input'
-import ToDoForm from './Input.Item'
+import React,{useState, useEffect} from 'react'
+import { Todolist } from './ToDoList/ToDolist';
 
-function App() {
-  const [todos, setTodos] = useState([])
-
-  const addTask = (userInput) => {
-    if(userInput) {
-      const newItem = {
-        id: Math.random().toString(36).substr(2,9),
-        task: userInput,
-        complete: false
-      }
-      setTodos([...todos, newItem])
-    }
+// getting the values of local storage
+const getDatafromLS=()=>{
+  const data = localStorage.getItem('books');
+  if (data){
+    return JSON.parse(data);
   }
-
-  const removeTask = (id) => {
-    setTodos([...todos.filter((todo) => todo.id !== id)])
+  else {
+    return []
   }
-
-  const handleToggle = (id) => {
-    setTodos([
-      ...todos.map((todo) => 
-        todo.id === id ? { ...todo, complete: !todo.complete } : {...todo }
-      )
-    ])
-  }
-
-  return (
-    <div className="App">
-      <header>
-        <h1>Task list: {todos.length}</h1>
-      </header>
-      <ToDoForm addTask={addTask} />
-      {todos.map((todo) => {
-        return (
-          <ToDo
-            todo={todo}
-            key={todo.id}
-            toggleTask={handleToggle}
-            removeTask={removeTask}
-            />
-        )
-      })}
-    </div>
-  );
 }
 
-export default App;
+export const App = () => {
+ 
+  // main array of objects state || books state || books array of objects
+  const [books, setbooks]=useState(getDatafromLS());
+
+  // input field states
+  const [title, setTitle]=useState('');
+  const [author, setAuthor]=useState('');
+  const [isbn, setIsbn]=useState('');
+
+  // form submit event
+  const handleAddBookSubmit=(e)=>{
+    e.preventDefault();
+    // creating an object
+    let book={
+      title,
+      author,
+      isbn
+    }
+    setbooks([...books,book]);
+    setTitle('');
+    setAuthor('');
+    setIsbn('');
+  }
+
+  // delete book from LS
+  const deleteBook=(isbn)=>{
+    const filteredBooks=books.filter((element,index)=>{
+      return element.isbn !== isbn
+    })
+    setbooks(filteredBooks);
+  }
+
+  // saving data to local storage
+  useEffect(()=>{
+    localStorage.setItem('books',JSON.stringify(books));
+  },[books])
+
+  return (
+    <div className='wrapper'>
+      <h1>BookList App</h1>
+      <div className='main'>
+
+        <div id='form-container'>
+          <form autoComplete="off" className='form-group'
+          onSubmit={handleAddBookSubmit}>
+            <input type="text" placeholder='Name...' required
+            onChange={(e)=>setTitle(e.target.value)} value={title}></input>
+            <br></br>
+            <input type="text" placeholder='Last Name...' required
+            onChange={(e)=>setAuthor(e.target.value)} value={author}></input>
+            <br/>
+            <input type="text" placeholder='Grate..' required
+            onChange={(e)=>setIsbn(e.target.value)} value={isbn}></input>
+            <br></br>
+            <button type="submit">
+              ADD
+            </button>
+          </form>
+        </div>
+
+        <div id='view-container'>
+          {books.length>0&&<>
+            <div id='table-responsive'>
+              <table id='table'>
+                <thead>
+                  <tr>
+                    <th>Name:</th>
+                    <th>Last Name:</th>
+                    <th>Grate:</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <Todolist books={books} deleteBook={deleteBook}/> 
+                </tbody>
+              </table>
+            </div>
+          </>}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
